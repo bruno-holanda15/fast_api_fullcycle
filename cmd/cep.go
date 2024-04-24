195d50c7a263723aee198e504325cac502e39cbf
Bruno Holanda <br.holanda15@gmail.com>
2024-04-24 19:53:39 -0300

    feat: option with flag command into cep subcommand

--- cmd/cep.go
+++ cmd/cep.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fast_api/app"
+	// "fmt"
 
 	"github.com/spf13/cobra"
 )
@@ -10,13 +11,20 @@ var cepCmd = &cobra.Command{
 	Use:   "cep",
 	Short: "Inform your cep to search",
 	Long:  "The cep passed will be used to call the APIs, and return its response",
-	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		cep := args[0]
+		// cep := args[0]
+		// app.CallAPIs(cep)
+		cep, err := cmd.Flags().GetString("command")
+		if err != nil {
+			panic(err)
+		}
+
 		app.CallAPIs(cep)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(cepCmd)
+	cepCmd.Flags().StringP("command", "c", "", "Informe o cep")
+	cepCmd.MarkFlagRequired("command")
 }
